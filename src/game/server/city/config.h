bf81b5cde586596c801cb32e1b34e5e6763054ae
Nemesis <webmaster@nugget.lol>
2020-06-29 23:33:11 +0200

    added own config file for city variables

---
+++ src/game/server/city/config.h
@@ -0,0 +1,11 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+#ifndef GAME_SERVER_CITY_CONFIG_H
+#define GAME_SERVER_CITY_CONFIG_H
+#undef GAME_SERVER_CITY_CONFIG_H // this file will be included several times
+
+// - fast map download
+MACRO_CONFIG_INT(SvMapWindow, sv_map_window, 15, 0, 100, CFGFLAG_SERVER, "Map downloading send-ahead window")
+MACRO_CONFIG_INT(SvFastDownload, sv_fast_download, 1, 0, 1, CFGFLAG_SERVER, "Enables fast download of maps")
+
+#endif
